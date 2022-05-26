7fcaa1585fb9c7c8938e08d1f3096d630493bc45
zaphkie1 <cisco0136h@protonmail.com>
2022-05-25 23:49:55 -0500

    Nuevo

---
+++ prueba/Modelo.java
@@ -0,0 +1,10 @@
+package prueba;
+
+import javax.swing.JOptionPane;
+
+public class Modelo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Estas seguro", "prueba", JOptionPane.ERROR_MESSAGE);
+        
+    }
+}
